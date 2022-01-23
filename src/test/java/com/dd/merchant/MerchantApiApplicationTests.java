fdbb5f43d07c074fd3168a47b42aef64c81fc436
Sajeev Palakkat Ramakrishnan <sajurrk@gmail.com>
2022-01-23 23:15:58 +1100

    updated integration testcase to run with in memory H2

--- src/test/java/com/dd/merchant/MerchantApiApplicationTests.java
+++ src/test/java/com/dd/merchant/MerchantApiApplicationTests.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -29,6 +30,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 		webEnvironment = SpringBootTest.WebEnvironment.MOCK,
 		classes = MerchantApiApplication.class)
 @AutoConfigureMockMvc
+@TestPropertySource(
+		locations = "classpath:application.properties")
 class MerchantApiApplicationTests {
 
 	@Autowired
